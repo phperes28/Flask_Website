968e104112e706a45110cb3dfee27b18df159633
Sckhar <pedrosilva28@gmail.com>
2021-10-28 15:05:54 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from flask_mail import Mail, Message
+from datetime import date
+
+
+app = Flask(__name__)
+
+
+mail = Mail(app)
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
