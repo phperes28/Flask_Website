226f25ebc047bdbab2df323b3ce41037827e76ca
Sckhar <pedrosilva28@gmail.com>
2021-11-17 21:09:41 -0300

    Formulario do template trocado por wtform e flask enviando email. Falta arrumar CSS do dorm

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, redirect, url_for, flash, request, abo
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_mail import Mail, Message
-from datetime import date
+from forms import ContactForm
+import os
+import smtplib
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+
 
 
 mail = Mail(app)
@@ -15,11 +19,33 @@ Bootstrap(app)
 
 
 
-@app.route("/")
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
-
-
+    form = ContactForm()
+    email = request.form.get("email")
+    name = request.form.get("name")
+    message = request.form.get("message")
+    if request.method == "POST":
+        if form.validate() == False:
+            flash('Preencha todos os campos')
+            return render_template('/', form=form)
+        else:
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login("pedro.tramit@gmail.com", "necrophagist289")
+            server.sendmail(from_addr= "pedro.tramit@gmail.com",
+            to_addrs= email,
+            msg=f'Subject: Novo contato@ \n\n{name} {email}\nEnviou a seguinte mensagem: {message}'
+            )
+            flash('Mensagem enviada!')
+            return redirect(url_for('home'))
+    return render_template("index.html", form=form)
 
 
 
